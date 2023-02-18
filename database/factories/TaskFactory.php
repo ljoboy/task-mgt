48ee3f2a72256b74ceda78552e232de310f703ea
Dark Angel <jonathanyombo@gmail.com>
2023-02-18 17:46:06 +0200

    task factory

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->sentence,
+        ];
+    }
+}
