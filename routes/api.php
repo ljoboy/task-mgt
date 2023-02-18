adc3e7a142dd2468fe7159387bf2cffb87ddf1f2
Dark Angel <jonathanyombo@gmail.com>
2023-02-18 23:20:25 +0200

    add routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\V1\ProjectAPIController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->as('api.v1.')->group(function () {
+    Route::prefix('projects')
+        ->as('projects.')
+        ->controller(ProjectAPIController::class)
+        ->group(function () {
+        Route::post('/', 'store')->name('store');
+    });
 });
