f5a88a8c1d1bb5c1b4019fe7f89369b931b2bedc
Dark Angel <jonathanyombo@gmail.com>
2023-02-18 23:16:55 +0200

    create a project

---
+++ app/Http/Controllers/API/V1/ProjectAPIController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\API\APIController;
+use App\Http\Requests\API\V1\Project\ProjectStoreRequest;
+use App\Http\Resources\API\V1\Project\ProjectResource;
+use App\Models\Project;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProjectAPIController extends APIController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): JsonResponse
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ProjectStoreRequest $request): JsonResponse
+    {
+        $project = Project::create($request->validated());
+
+        return $this->responseSuccess(
+            new ProjectResource($project),
+            'Project has been created',
+            Response::HTTP_CREATED
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Project $project): JsonResponse
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Project $project): JsonResponse
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Project $project): JsonResponse
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/API/V1/Project/ProjectStoreRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\API\V1\Project;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProjectStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+        ];
+    }
+}
