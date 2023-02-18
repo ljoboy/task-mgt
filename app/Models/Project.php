f6dd5f752f1c43deb5c1d31c3fc513c2ee2a257d
Dark Angel <jonathanyombo@gmail.com>
2023-02-18 16:56:25 +0200

    add a model for project

---
+++ app/Models/Project.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+}
