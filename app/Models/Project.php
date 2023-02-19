35aceac9887b04978acbb37d385e56f7322327c3
Dark Angel <jonathanyombo@gmail.com>
2023-02-19 12:25:00 +0200

    cast timestamps to datetime

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -12,16 +13,22 @@ class Project extends Model
 {
     use HasFactory;
 
-    protected array $dates = [
-        'created_at',
-        'updated_at',
-    ];
     protected $fillable = [
         'name'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
     ];
 
     public function tasks(): HasMany
     {
         return $this->hasMany(Task::class);
+    }
+
+    protected function serializeDate(DateTimeInterface $date): string
+    {
+        return $date->format('Y-m-d H:i:s');
     }
 }
