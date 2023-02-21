d4bda8aaec88287896c136feb6bc0797d2433f38
Dark Angel <jonathanyombo@gmail.com>
2023-02-21 17:10:17 +0200

    install and config Vue

---
+++ resources/js/components/Counter.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+    {{ count }}
+
+    <button @click="count++">
+        Count
+    </button>
+</template>
